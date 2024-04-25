54cf83239d651acc13abdee59cb3e02fd5990b1e
FantasySilence <1735793009@qq.com>
2024-04-26 03:46:49 +0800

    更新了文件IO流

--- common/figsio.py
+++ common/figsio.py
@@ -2,30 +2,51 @@ import os
 import matplotlib.pyplot as plt
 
 
-# TODO：更新图片保存工具
-def save_fig(fig_id: str, tight_layout: bool=False, 
-             suffix: str="png", dpi: int=300) -> None:
+class FiguresIO:
 
     """
-    工具函数，用于图片的保存
+    实现一个图片IO流类用于保存和读取图片(路径)
     """
 
-    subPath = os.path.dirname(os.path.dirname(__file__))
-    FIGPATH = os.path.join(subPath, "figures")
-    SAVEPATH = os.path.join(FIGPATH, fig_id)
-    print("Saving figure", fig_id)
-    if tight_layout:
+    @staticmethod
+    def saveFigures(fig_name: str, lecture_name: str) -> None:
+
+        """
+        保存图片
+        """
+
+        # common文件夹路径
+        common_path = os.path.dirname(__file__)
+        # 项目根路径
+        root_path = os.path.dirname(common_path)
+        # figures文件夹路径
+        figures_path = os.path.join(root_path, "figures")
+
+        # ------ 创建存放对应lecture图片的文件夹 ------ #
+        lecture_fig_path = os.path.join(figures_path, lecture_name)
+        if not os.path.exists(lecture_fig_path):
+            os.makedirs(lecture_fig_path)
+        else:
+            pass
+
+        # ------ 保存图片 ------ #
+        figsave_path = os.path.join(lecture_fig_path, fig_name)
+        print("save figure %s..." % fig_name)
         plt.tight_layout()
-    plt.savefig(SAVEPATH + ".png", format=suffix, dpi=dpi)
+        plt.savefig(figsave_path, dpi=300)
 
 
-def getFigPath(fig_id: str) -> str:
+    @staticmethod
+    def loadFigurePath(fig_name: str) -> str:
 
-    """
-    获取图片所在路径
-    """
+        """
+        读取图片路径
+        """
 
-    subPath = os.path.dirname(os.path.dirname(__file__))
-    FIGPATH = os.path.join(subPath, "figures")
-    SAVEPATH = os.path.join(FIGPATH, fig_id)
-    return SAVEPATH
+        # common文件夹路径
+        common_path = os.path.dirname(__file__)
+        # 项目根路径
+        root_path = os.path.dirname(common_path)
+        # figures文件夹路径
+        figures_path = os.path.join(root_path, "figures")
+        return os.path.join(figures_path, fig_name)
